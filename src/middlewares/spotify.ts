5fd47aecc70559c0ccfa8133ddaad971462d7428
Manish Devrani <manishdevrani777@gmail.com>
2025-07-13 12:20:06 +0530

    feat: spotify-auth-token-middleware

---
+++ src/middlewares/spotify.ts
@@ -0,0 +1,27 @@
+import { NextFunction, Request, Response } from "express";
+
+const SPOTIFY_CLIENT_ID = process.env.SPOTIFY_CLIENT_ID!;
+const SPOTIFY_CLIENT_SECRET = process.env.SPOTIFY_CLIENT_SECRET!;
+
+export async function spotifyAuthMiddleware(req: Request, res: Response, next: NextFunction) {
+  try {
+    const res = await fetch(`https://accounts.spotify.com/api/token?grant_type=client_credentials&client_id=${SPOTIFY_CLIENT_ID}&client_secret=${SPOTIFY_CLIENT_SECRET}`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded"
+      }
+    });
+
+    const data = await res.json();
+
+    if (res.status === 200) {
+      req.spotifyToken = data.access_token;
+      next();
+    }
+  } catch (err) {
+    console.log("error occured: ", err);
+    res.status(500).json({
+      message: "failed to authorize"
+    })
+  }
+}
--- src/server.ts
+++ src/server.ts
@@ -2,14 +2,16 @@ import "dotenv/config";
 import express from "express";
 
 import { healthCheckRouter } from "./routes/health-check";
+import { spotifyAuthMiddleware } from "./middlewares/spotify";
 
 const PORT = process.env.PORT ?? 8000;
 
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded());
 
-app.use("/api/v1/healthcheck", healthCheckRouter);
+app.use("/api/v1/healthcheck", spotifyAuthMiddleware, healthCheckRouter);
 
 app.listen(PORT, () => {
   console.log(`Listening on PORT: ${PORT}`);
